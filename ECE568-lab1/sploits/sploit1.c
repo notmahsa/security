3bc872e8b156b938bec3a35285c7ffdb152e38e3
Mahsa Naserifar <mahsa@render.com>
2020-01-29 21:26:20 -0500

    Test print

--- ECE568-lab1/sploits/sploit1.c
+++ ECE568-lab1/sploits/sploit1.c
@@ -6,10 +6,10 @@
 
 #define TARGET "../targets/target1"
 
-#define SHELL_SIZE 46
+#define SHELL_SIZE 45
 #define BUF_TO_RET 120
 #define BUF_SIZE 125
-#define BUF_ADDR 0x2021feb0
+#define BUF_ADDR 0x2021fe10
 
 int
 main ( int argc, char * argv[] )
