230a89c470cda31816f34b9ff5c9eec420928388
Mahsa Naserifar <mahsa@render.com>
2020-04-07 14:11:15 -0400

    Testing spoofed example.com

--- Lab4/dnsproxy_starter.py
+++ Lab4/dnsproxy_starter.py
@@ -46,8 +46,8 @@ def handler(data, addr, socket, dns_ip):
             url = original_dns_packet[DNS].qd.qname
             if url in to_be_spoofed:
                 print "Request for %s will be spoofed" % url[:-2]
-                spoofed_dns_packet = IP(dst=original_dns_packet[IP].dst, src=original_dns_packet[IP].src) /
-                    UDP(dport=original_dns_packet[UDP].dport, sport=original_dns_packet[UDP].sport) /
+                spoofed_dns_packet = IP(dst=original_dns_packet[IP].dst, src=original_dns_packet[IP].src) /\
+                    UDP(dport=original_dns_packet[UDP].dport, sport=original_dns_packet[UDP].sport) /\
                     DNS(id=original_dns_packet[DNS].id, qr=1, aa=1, qd=original_dns_packet[DNS].qd,
                     an=DNSRR(rrname=original_dns_packet[DNS].qd.qname, ttl=original_dns_packet[DNS].an.ttl,rdata=to_be_spoofed[url]['ipv4']),
                     ns=DNSRR(rrname=original_dns_packet[DNS].qd.qname, ttl=84107, type=NS, rdadta=to_be_spoofed[url]['ns']))
