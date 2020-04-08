47a99a55e3fd3531d3de62876cc7af57c6dd4b0d
Mahsa Naserifar <mahsa@render.com>
2020-04-07 22:46:49 -0400

    Added attack - debugging match

--- Lab4/part4_starter.py
+++ Lab4/part4_starter.py
@@ -102,7 +102,7 @@ def attack():
             else:
                 print "Poisonning on %s failed, ns is %s" % (url, str(response[DNS].ns))
         except:
-            print "Poisonning on %s failed, response is \n%s" % (url, response.show())
+            print "Poisonning on %s failed, response is \n%s" % (url)
     
 
 if __name__ == '__main__':
