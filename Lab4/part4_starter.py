8ce067f4d829334bb81bcf828011584e9da8ac6f
Mahsa Naserifar <mahsa@render.com>
2020-04-07 22:43:04 -0400

    Added attack - debugging match

--- Lab4/part4_starter.py
+++ Lab4/part4_starter.py
@@ -92,6 +92,7 @@ def attack():
         dns_request[DNS].qd.qname = base_domain
         sendPacket(sock, dns_request, my_ip, dns_port)
         response = sock.recv(4096)
+        print "LOL", response[2:]
         response = DNS(response[2:])
         try:
             if response[DNS].an:
